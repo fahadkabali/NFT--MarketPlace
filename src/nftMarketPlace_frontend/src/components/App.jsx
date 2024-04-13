787a77ac5a1d2858df99da7f6443c01bc43e879e
fahadkabali <kabalifahad@gmail.com>
2024-04-14 01:20:17 +0300

    update

--- src/nftMarketPlace_frontend/src/components/App.jsx
+++ src/nftMarketPlace_frontend/src/components/App.jsx
@@ -3,12 +3,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../public/home-img.png";
+import Item from "./Item";
 
 function App() {
+  const  NFTID = "bkyz2-fmaaa-aaaaa-qaaaq-cai";
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      <Item id={NFTID}/>
+      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
